7738dd5bcb04b4451f3f04cad3cde5b628d9a65d
Maxuel Vieira Toba Junior <maxueljunior@weg.net>
2023-01-20 13:42:59 -0300

    Aula dia 20/01/23 serviço

--- 17_AutenticacaoJWTeSpringSecurity/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
+++ 17_AutenticacaoJWTeSpringSecurity/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import br.com.erudio.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-	@Query("SELECT u FROM User WHERE u.userName =:userName")
+	@Query("SELECT u FROM User u WHERE u.userName =:userName")
 	User findByUsername(@Param("userName") String userName);
 	
 }
