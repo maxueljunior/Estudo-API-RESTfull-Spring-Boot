75e70679e9facd48d29fe6ab147f20524ca44990
Maxuel Vieira Toba Junior <maxueljunior@weg.net>
2023-02-08 15:55:48 -0300

    aula dia 08.02 firma

--- 23_ConsumindoAPIReact/rest-with-spring-boot-and-java-erudio/client/src/App.js
+++ 23_ConsumindoAPIReact/rest-with-spring-boot-and-java-erudio/client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './Header'
+import Login from './pages/Login'
 
 export default function App() {
   return (
-    <Header>Client REST Udemy - Properties</Header>
+    <Login/>
   );
 }
