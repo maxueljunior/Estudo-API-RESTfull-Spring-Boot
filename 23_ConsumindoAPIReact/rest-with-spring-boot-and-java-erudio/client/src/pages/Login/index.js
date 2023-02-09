49a75b7fb10406ff2c6e07a7b76e227acba7ad5c
Maxuel Vieira Toba Junior <maxueljunior@weg.net>
2023-02-09 13:12:09 -0300

    Aula firma 09.02

--- 23_ConsumindoAPIReact/rest-with-spring-boot-and-java-erudio/client/src/pages/Login/index.js
+++ 23_ConsumindoAPIReact/rest-with-spring-boot-and-java-erudio/client/src/pages/Login/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './styles.css';
+import logoImage from '../../assets/logo.svg'
+import padlock from '../../assets/padlock.png'
 
 export default function Login(){
     return(
-        <h1>Welcome User Maxuel</h1>
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Erudio Logo"/>
+                <form>
+                    <h1>Acesse a sua conta</h1>
+                    <input placeholder="Username"/>
+                    <input type="password" placeholder="Password"/>
+                    <button className="button" type="submit">Login</button>
+                </form>
+            </section>
+            <img src={padlock} alt="Login"/>
+        </div>
     )
 }
